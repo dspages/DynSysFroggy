b0573925d321235ea31e58f4d834ee757828f807
Andrew Booth <boothandrewd@gmail.com>
2017-08-22 14:21:18 -0700

    Merge stuff

---
+++ server/__init__.py
@@ -0,0 +1,8 @@
+""" app/__init__.py
+"""
+from flask import Flask
+
+server = Flask(__name__)
+server.config.from_pyfile('config.py')
+
+from server import views
---
+++ server/views.py
@@ -0,0 +1,9 @@
+""" app/views.py
+"""
+from flask import render_template
+
+from server import server
+
+@server.route('/')
+def index():
+    return render_template('index.html')
