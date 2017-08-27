35f238c9d29d33927ae45b96f465e77286021eab
%= GitConfig.user_name % <%= GitConfig.user_email %>
2017-08-27 09:57:51 -0700

    did a little bit of refactoring and moved dead code to notes.js in docs folder

--- server/static/js/bundle.js
+++ server/static/js/bundle.js
@@ -9764,65 +9764,9 @@ var _display2 = _interopRequireDefault(_display);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var SCALE = 50.0; // var algebra = require('../vendor/algebra-0.2.6.min.js');
-
-
-function getVector(x, y) {
-  // console.log(x);
-  // console.log(y);
-  // console.log("getting vector");
-  // var dxdt = new algebra.Expression("x");
-  // dxdt = dxdt.multiply("x");
-  // dxdt = dxdt.subtract("y");
-  //
-  // var dydt = new algebra.Expression("x");
-  // dydt = dydt.subtract("y");
-  // return [(y*x)+1,x-y];
-  // return [-x+2+y,(-x*y)-y];
-  return [Math.cos(y * y) * 5, Math.sin(x - y) * 5];
-  // return [Math.cos(x-y),x-y];
-  // return [(x*x)-y,x-y];
-  // return [(y*x)-(y*y*.2),(y*x)-(x*x*.5)];
-}
+var SCALE = 50.0;
 
 document.addEventListener("DOMContentLoaded", function () {
-  // const canvasEl = document.getElementsByTagName("canvas")[0];
-  // canvasEl.width = 600;
-  // canvasEl.height = 600;
-  // var ctx=canvasEl.getContext("2d");
-  // let arr=[];
-  // for (var i = -6; i <= 6; i++) {
-  //   arr.push([]);
-  //   for (var j = -6; j <= 6; j++) {
-  //     let vector=getVector(i,j);
-  //     arr[i+6][j+6]=vector;
-  //     line([i,j],
-  //     [(i)+parseFloat(vector[0])/SCALE,(j)+parseFloat(vector[1])/SCALE],ctx);
-  //   }
-  // }
-  // console.log(arr);
-  //
-  //
-  // let trajectoryx=[-5,-1,1,2,0.5,.5,5,5, -3, -4, 5.5];//set initial conditions
-  // let trajectoryy=[5,5,3,3,0.5,0,-5,5, -3, -1, -4];//set initial conditions
-  // let oldVec = [];
-  // let newVec = [];
-  // for (var j = 0; j < trajectoryx.length; j++) {
-  //   oldVec=[trajectoryx[j], trajectoryy[j]];
-  //   for (var i = 0; i<200; i++) {
-  //     let x = parseFloat(oldVec[0]);
-  //     let y = parseFloat(oldVec[1]);
-  //     newVec=getVector(x,y);
-  //     newVec[0]/=SCALE;
-  //     newVec[1]/=SCALE;
-  //     newVec[0]+=x;
-  //     newVec[1]+=y;
-  //     console.log(oldVec);
-  //     console.log(newVec);
-  //     line(oldVec,newVec,ctx, "red");
-  //     oldVec=newVec;
-  //   }
-  // }
   var root = document.getElementById("root");
   _reactDom2.default.render(_react2.default.createElement(
     'div',
@@ -22496,7 +22440,7 @@ var Display = function (_React$Component) {
 
     var _this = _possibleConstructorReturn(this, (Display.__proto__ || Object.getPrototypeOf(Display)).call(this, props));
 
-    _this.state = { dxdt: "y*y-x", dydt: "x-y", length: 0.05 };
+    _this.state = { dxdt: "y*y-x", dydt: "x-y", length: 0.02 };
     _this.updateGraph = _this.updateGraph.bind(_this);
     _this.changedxdt = _this.changedxdt.bind(_this);
     _this.changedydt = _this.changedydt.bind(_this);
